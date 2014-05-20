17ee637f6cceb7b47a4d4466bb1404f20b799cbb
Sean Devine <barelyknown@icloud.com>
2014-05-20 02:19:12 -0500

    make retweeting worker smarter with some ugly code

--- app/workers/tweet_retweeting_worker.rb
+++ app/workers/tweet_retweeting_worker.rb
@@ -5,11 +5,20 @@ class TweetRetweetingWorker
   recurrence { minutely(5) }
 
   def perform
-    tweet_id = REDIS.zrange(Searcher::RETWEETABLE_SET_KEY, 0, 0).first
-    if tweet_id.present?
-      TWITTER.retweet tweet_id
-      REDIS.zrem Searcher::RETWEETABLE_SET_KEY, tweet_id.to_s
-      REDIS.zadd 'retweeted-tweets', Time.now.utc.to_i, tweet_id.to_s
+    retweeted, no_more_tweets = false, false
+    until retweeted || no_more_tweets
+      tweet_id = REDIS.zrange(Searcher::RETWEETABLE_SET_KEY, 0, 0).first
+      if tweet_id.present?
+        tweet = TWITTER.status(tweet_id)
+        unless Filter.new(tweet).filter?
+          TWITTER.retweet tweet_id
+          REDIS.zadd 'retweeted-tweets', Time.now.utc.to_i, tweet_id.to_s
+          retweeted = true
+        end
+        REDIS.zrem Searcher::RETWEETABLE_SET_KEY, tweet_id.to_s
+      else
+        no_more_tweets = true
+      end
     end
   end
 
