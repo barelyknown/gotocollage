66e0a9f174e00c95bb858135bab4fd339ba91083
Sean Devine <barelyknown@icloud.com>
2014-05-20 01:24:14 -0500

    guard against null tweet_id

--- app/workers/tweet_retweeting_worker.rb
+++ app/workers/tweet_retweeting_worker.rb
@@ -6,9 +6,11 @@ class TweetRetweetingWorker
 
   def perform
     tweet_id = REDIS.zrange(Searcher::RETWEETABLE_SET_KEY, 0, 0).first
-    TWITTER.retweet tweet_id
-    REDIS.zrem Searcher::RETWEETABLE_SET_KEY, tweet_id.to_s
-    REDIS.zadd 'retweeted-tweets', Time.now.utc.to_i, tweet_id.to_s
+    if tweet_id.present?
+      TWITTER.retweet tweet_id
+      REDIS.zrem Searcher::RETWEETABLE_SET_KEY, tweet_id.to_s
+      REDIS.zadd 'retweeted-tweets', Time.now.utc.to_i, tweet_id.to_s
+    end
   end
 
 end
