8d9c365d95daf11852b9f36945b6d68ad198e075
Sean Devine <barelyknown@icloud.com>
2014-05-20 08:51:50 -0500

    more filtering

--- app/models/filter.rb
+++ app/models/filter.rb
@@ -1,6 +1,6 @@
 class Filter
-  SWEARS = %w(shit fuck ass dick cunt pussy bitch)
-  SLURS = %w(fag gay homo nigga nigger nig)
+  SWEARS = %w(shit fuck ass dick cunt pussy)
+  SLURS = %w(fag gay homo nigga nigger nig bitch twat)
 
   attr_reader :tweet
 
@@ -11,30 +11,25 @@ class Filter
   def filter?
     return true unless includes_exact_phrase?
     return true unless non_reply?
-    return true unless excludes_swears?
-    return true unless excludes_slurs?
+    return true unless excludes_profanity?
     return true unless not_retweeted_old_school_style?
     return true unless not_quoted?
     false
   end
 
   def includes_exact_phrase?
-    tweet.text =~ /go to collage/
+    tweet.text =~ /go to collage/i
   end
 
   def non_reply?
     tweet.text =~ /\A[^@]/
   end
 
-  def excludes_swears?
-    SWEARS.none? do |swear|
-      tweet.text =~ /#{swear}/i
-    end
-  end
-
-  def excludes_slurs?
-    SLURS.none? do |slur|
-      tweet.text =~ /#{slur}/i
+  def excludes_profanity?
+    (SWEARS + SLURS).none? do |profanity|
+      [tweet.text, tweet.user.name, tweet.user.screen_name].none? do |attribute|
+        attribute =~ /#{profanity}/i
+      end
     end
   end
 
--- spec/models/filter_spec.rb
+++ spec/models/filter_spec.rb
@@ -2,40 +2,56 @@ require 'spec_helper'
 
 describe Filter do
 
+  let(:safe_user) do
+    double(Twitter::User, name: 'Hello Kitty', screen_name: 'hello_kitty')
+  end
+
   it "filters tweets that do not say 'go to collage'" do
-    tweet = double(Twitter::Tweet, text: 'going to collage')
+    tweet = double(Twitter::Tweet, text: 'going to collage', user: safe_user)
     expect(described_class.new(tweet)).to be_filter
   end
 
   it 'filters reply tweets that start with an @' do
-    tweet = double(Twitter::Tweet, text: "@you I'm go to collage")
+    tweet = double(Twitter::Tweet, text: "@you I'm go to collage", user: safe_user)
     expect(described_class.new(tweet)).to be_filter
   end
 
   it 'filters tweets that include profanity' do
     Filter::SWEARS.each do |swear|
-      tweet = double(Twitter::Tweet, text: "go to collage #{swear.upcase}")
+      tweet = double(Twitter::Tweet, text: "go to collage #{swear.upcase}", user: safe_user)
       expect(described_class.new(tweet)).to be_filter
     end
   end
 
   it 'filters tweets that include anything racial' do
     Filter::SLURS.each do |slur|
-      tweet = double(Twitter::Tweet, text: "go to collage #{slur.upcase}")
+      tweet = double(Twitter::Tweet, text: "go to collage #{slur.upcase}", user: safe_user)
       expect(described_class.new(tweet)).to be_filter
     end
   end
 
   it 'filters tweets that start with RT' do
-    tweet = double(Twitter::Tweet, text: "RT go to collage")
+    tweet = double(Twitter::Tweet, text: "RT go to collage", user: safe_user)
     expect(described_class.new(tweet)).to be_filter
   end
 
   it 'filters tweets that start with quotes' do
     %w(' " “).each do |q|
-      tweet = double(Twitter::Tweet, text: "#{q}go to collage'")
+      tweet = double(Twitter::Tweet, text: "#{q}go to collage'", user: safe_user)
       expect(described_class.new(tweet)).to be_filter
     end
+  end
+
+  it 'filters tweets from users with name with profanity' do
+    user = double(Twitter::User, name: Filter::SWEARS.first, screen_name: 'hello_kitty')
+    tweet = double(Twitter::Tweet, text: 'go to collage', user: user)
+    expect(described_class.new(tweet)).to be_filter
+  end
+
+  it 'filters tweets from users with screen_names with profanity' do
+    user = double(Twitter::User, screen_name: Filter::SWEARS.first, name: 'Hello Kitty')
+    tweet = double(Twitter::Tweet, text: 'go to collage', user: user)
+    expect(described_class.new(tweet)).to be_filter
   end
 
 end
